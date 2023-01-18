84f422d2558578a466ebbc60fab0c6d33ae52455
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-18 21:45:30 +0200

    Dispatch AddCustomer

--- src/components/ReservationCard.tsx
+++ src/components/ReservationCard.tsx
@@ -1,5 +1,7 @@
 import { useDispatch } from "react-redux";
 import { removeReservation } from "../features/reservationsSlice";
+import { addCustomer } from "../features/customerSlice";
+import { v4 as uuid } from "uuid";
 
 interface ReservationCardTypes {
   name: string;
@@ -14,6 +16,13 @@ export const ReservationCard = ({ name, index }: ReservationCardTypes) => {
       className="reservation-card-container"
       onClick={() => {
         dispatch(removeReservation(index));
+        dispatch(
+          addCustomer({
+            id: uuid(),
+            name,
+            food: [],
+          })
+        );
       }}
     >
       {name}
