ab55d855d4f0e0b16bd00c41e162e077482092c3
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-18 21:14:54 +0200

    Add reservationSlice reducers

--- src/features/reservationsSlice.ts
+++ src/features/reservationsSlice.ts
@@ -1,13 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
+export interface ReservationsState {
+  value: string[];
+}
+
+const initialState: ReservationsState = {
   value: [],
 };
 
 export const reservationsSlice = createSlice({
   name: "reservations",
   initialState,
-  reducers: {},
+  reducers: {
+    addReservation: (state, action: PayloadAction<string>) => {
+      state.value.push(action.payload);
+    },
+    removeReservation: (state, action: PayloadAction<number>) => {
+      state.value.splice(action.payload, 1);
+    },
+  },
 });
 
-export default reservationsSlice.reducer;
+export const { addReservation, removeReservation } = reservationsSlice.actions;
+
+export default reservationsSlice.reducer; 
